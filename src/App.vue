4bebc4c9861cf84c7c16268958f881830e1eda5d
Muhammad Usman <muhammad@weebly.com>
2018-01-21 17:12:18 -0800

    getting set up with other people's photos, these will not be final

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:800|Heebo:500,700,800,900|Neuton|Vollkorn" rel="stylesheet">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+h1 {
+  margin: 50px 0;
+}
+#app {
+  font-family: Catamaran, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <ul class="full-list">
+      <li>
+        <span class="name">
+          Cilantro
+        </span>
+        <span>
+          <img src="/static/images/cilantro.png" alt="Illustration of Cilantro">
+        </span>
+      </li>
+      <li>
+        <span class="name">
+          Mint
+        </span>
+        <span>
+          <img src="/static/images/mint.png" alt="Illustration of Mint">
+        </span>
+      </li>
+      <li>
+        <span class="name">
+          Thyme
+        </span>
+        <span>
+          <img src="/static/images/thyme.png" alt="Illustration of Thyme">
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      title: 'Herbs & Spices'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+.full-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  li {
+    list-style: none;
+    border: 1px solid rgba(123, 123, 123, 0.4);
+    width: 124px;
+    height: 124px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 13px;
+    box-shadow: 0px 0px 10px rgba(123, 123, 123, 0.1);
+    .name {
+      font-size: 18px;
+      margin: 10px 0;
+    }
+    img {
+      width: 40px;
+    }
+  }
+}
+</style>
